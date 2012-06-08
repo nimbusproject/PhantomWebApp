e3a7a1751376a25716b49701df489b776f16d1a3
BuzzTroll <buzztroll@gmail.com>
2012-06-08 12:39:12 -0500

    attempting to get the email passwor parts working

--- phantomweb/urls.py
+++ phantomweb/urls.py
@@ -7,12 +7,14 @@ from django.contrib.auth.views import password_reset, password_change, password_
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
+    url(r'^accounts/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name' : 'registration/password_reset.html',  'post_reset_redirect': '/logout/' }),
+
     url(r'^accounts/change_password/$', password_change, {
         'post_change_redirect' : '/accounts/change_password/done/'})                                                                            ,
     url(r'^accounts/change_password/done/$', password_change_done),
     (r'^accounts/reset_password/$', password_reset),
     (r'^accounts/reset_password/done$', password_reset_done),
-    url(r'^accounts/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm),
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
