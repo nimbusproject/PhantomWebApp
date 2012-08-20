8f3e9c167200424cdcee39892207ab8ec47742a9
BuzzTroll <buzztroll@gmail.com>
2012-08-20 14:05:12 -0500

    changing the size of the loading image

--- phantomweb/static/js/phantom.js
+++ phantomweb/static/js/phantom.js
@@ -25,29 +25,6 @@ function make_url(p) {
     return base_url.concat(p);
 }
 
-function get_ajax_obj() {
-    var ajaxRequest;  // The variable that makes Ajax possible!
-
-    try{
-        // Opera 8.0+, Firefox, Safari
-        ajaxRequest = new XMLHttpRequest();
-    } catch (e){
-        // Internet Explorer Browsers
-        try{
-            ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            try{
-                ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e){
-                // Something went wrong
-                alert("Your browser broke!");
-                return false;
-            }
-        }
-    }
-    return ajaxRequest;
-}
-
 function std_error_handler(url, error_msg) {
     var errorOpt = document.getElementById('error_status_text');
     errorOpt.innerText = error_msg;
