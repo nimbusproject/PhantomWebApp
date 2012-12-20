97f0d660badaa07f7509c0273b8b979ae2a2f5ac
Pierre Riteau <priteau@ci.uchicago.edu>
2012-12-20 18:34:23 +0000

    Script names were inverted

--- bin/phantomweb-list-users.py
+++ bin/phantomweb-list-users.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 
 import os
-import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'phantomweb.settings'
 
 from django.contrib.auth.models import User
 
-if len(sys.argv) != 4:
-    sys.exit("usage: %s username email password" % sys.argv[0])
-
-user = User.objects.create_user(sys.argv[1], sys.argv[2], sys.argv[3])
-user.is_staff = False
-user.is_superuser = False
-user.save()
+users = User.objects.all()
+for i in users:
+    print i
--- bin/phantomweb-new-user.py
+++ bin/phantomweb-new-user.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'phantomweb.settings'
 
 from django.contrib.auth.models import User
 
-users = User.objects.all()
-for i in users:
-    print i
+if len(sys.argv) != 4:
+    sys.exit("usage: %s username email password" % sys.argv[0])
+
+user = User.objects.create_user(sys.argv[1], sys.argv[2], sys.argv[3])
+user.is_staff = False
+user.is_superuser = False
+user.save()
