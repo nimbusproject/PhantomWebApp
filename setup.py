d6decd6c242c9b245eab6e76fce75a0882aa93a1
Pierre Riteau <priteau@ci.uchicago.edu>
2013-02-20 17:28:33 +0000

    Stick to Django 1.4.3 as the 1.4.4 tarball doesn't work
    See https://code.djangoproject.com/ticket/19858

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name=Name,
       packages=['phantomweb', 'phantomweb.templatetags'],
       include_package_data=True,
       package_data={ 'phantomweb': ['templates/registration/*.html', 'templates/*.html', 'static/css/*', 'static/js/*', 'static/images/*']  },
-      install_requires = ["django >= 1.4, < 1.5", "boto == 2.6", "phantomsql == 0.2.1", "ceiclient"],
+      install_requires = ["django == 1.4.3", "boto == 2.6", "phantomsql == 0.2.1", "ceiclient"],
       dependency_links=[
           "http://github.com/nimbusproject/PhantomSQL/tarball/master#egg=phantomsql-0.2.1",
           "http://github.com/nimbusproject/ceiclient/tarball/master#egg=ceiclient-0.1"],
