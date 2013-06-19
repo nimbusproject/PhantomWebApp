6274ba268ab8db4c80cb107b62b1fa78d19592c0
Pierre Riteau <priteau@ci.uchicago.edu>
2013-06-19 11:51:31 +0100

    Link PhantomUser model to the admin panel and new user script

--- bin/phantomweb-new-user.py
+++ bin/phantomweb-new-user.py
@@ -7,10 +7,14 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'phantomweb.settings'
 
 from django.contrib.auth.models import User
 
-if len(sys.argv) != 4:
-    sys.exit("usage: %s username email password" % sys.argv[0])
+from phantomweb.models import PhantomUser
 
-user = User.objects.create_user(sys.argv[1], sys.argv[2], sys.argv[3])
+if len(sys.argv) != 5:
+    sys.exit("usage: %s username email access_key_id password" % sys.argv[0])
+
+user = User.objects.create_user(sys.argv[1], sys.argv[2], sys.argv[4])
 user.is_staff = False
 user.is_superuser = False
 user.save()
+phantom_user = PhantomUser.objects.create(username=sys.argv[1], access_key_id=sys.argv[3])
+phantom_user.save()
--- phantomweb/admin.py
+++ phantomweb/admin.py
@@ -1,18 +1,33 @@
 from django.contrib import admin
-from phantomweb.models import PhantomInfoDB, RabbitInfoDB, LaunchConfigurationDB, HostMaxPairDB
+
+from phantomweb.models import PhantomInfoDB, RabbitInfoDB, LaunchConfigurationDB, HostMaxPairDB, PhantomUser
+
 
 class PhantomInfoAdmin(admin.ModelAdmin):
     pass
+
 admin.site.register(PhantomInfoDB, PhantomInfoAdmin)
+
 
 class RabbitInfoAdmin(admin.ModelAdmin):
     pass
+
 admin.site.register(RabbitInfoDB, RabbitInfoAdmin)
+
 
 class LaunchConfigurationAdmin(admin.ModelAdmin):
     pass
+
 admin.site.register(LaunchConfigurationDB, LaunchConfigurationAdmin)
+
 
 class HostMaxPairAdmin(admin.ModelAdmin):
     pass
+
 admin.site.register(HostMaxPairDB, HostMaxPairAdmin)
+
+
+class PhantomUserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PhantomUser, PhantomUserAdmin)
