31a1f769cb9fc185e8027279dc7e239c349450b7
Patrick Armstrong <patricka@uvic.ca>
2013-07-03 10:07:17 -0700

    WIP

--- phantomweb/static/js/phantom_public_lc.js
+++ phantomweb/static/js/phantom_public_lc.js
@@ -15,6 +15,12 @@ $(document).ready(function() {
         return false;
     });
 
+    $("#public_lc_table").on("click", "button.import", function() {
+        var id = $(this).data('name');
+        import_lc(id);
+        return false;
+    });
+
     $(window).on('hashchange', function() {
         select_from_hash();
     });
@@ -26,7 +32,7 @@ function load_public_lcs() {
     var load_lc_success = function(launchconfigs) {
         for (var i=0; i<launchconfigs.length; i++) {
             var lc = launchconfigs[i];
-            available_launch_configs.push(lc.id);
+            available_launch_configs[lc.id] = lc;
             make_lc_row(lc);
         }
 
@@ -42,10 +48,33 @@ function load_public_lcs() {
     phantomGET(lc_url, load_lc_success, load_lc_failure);
 }
 
+function import_lc(lc_id_to_import) {
+    var import_lc_success = function(imported_lc) {
+        window.location.href = "/phantom/launchconfig#" + imported_lc.name;
+    }
+
+    var import_lc_failure = function(error) {
+        phantom_error("Problem importing launch config! " + error);
+        console.log(error);
+    }
+
+    var lc_to_import = available_launch_configs[lc_id_to_import];
+    lc_to_import['name'] = 'import'; //TODO: DELETEME
+    delete lc_to_import['id'];
+    delete lc_to_import['url'];
+    delete lc_to_import['owner'];
+    delete lc_to_import['description'];
+
+    var url = make_url("launchconfigurations");
+    phantomPOST(url, lc_to_import, import_lc_success, import_lc_failure);
+    $("#row-" + lc_id_to_import + " button.import").html("Importing...");
+}
+
 
 function make_lc_row(lc) {
 
-    var row = "<tr id='row-" + lc.id + "'><td><p class='lead'><a class='lc_id' href='#" + lc.id + "'>" + lc.name + "</a></p>" +
+    var row = "<tr id='row-" + lc.id + "'>" +
+        "<td><p class='lead'><a class='lc_id' href='#" + lc.id + "'>" + lc.name + "</a></p>" +
         "<p>By " + lc.owner + "</p>" +
         "<p><button class='btn btn-small import' data-name='" + lc.id + "'>Import</button></p></td>" +
         "<td><p class='lead'>&nbsp;</p><p>" + lc.description + "</td>" +
@@ -55,8 +84,7 @@ function make_lc_row(lc) {
 
 function select_from_hash() {
     var url_id = window.location.hash.substring(1);
-    console.log(url_id);
-    if (available_launch_configs.indexOf(url_id) > -1) {
+    if (available_launch_configs.hasOwnProperty(url_id)) {
         $("#public_lc_table tr").hide();
         $("#row-" + url_id).show();
         $("#show-all-lc").show();
