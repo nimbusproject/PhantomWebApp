d2d47ef79943f5fe1e3b0f6e8fadcbfb0bb8c4ec
Pierre Riteau <priteau@uchicago.edu>
2014-06-02 17:33:17 +0100

    Force non-empty value

--- phantomweb/workload.py
+++ phantomweb/workload.py
@@ -131,6 +131,8 @@ def add_packer_credentials(username, cloud, nimbus_user_cert=None, nimbus_user_k
                     key=nimbus_user_key, canonical_id=nimbus_canonical_id, openstack_username=" ", openstack_password=" ",
                     openstack_project=" ")
         elif openstack_username is not None:
+            if openstack_password is None:
+                openstack_password = " "
             pc = PackerCredential.objects.create(username=username, cloud=cloud, certificate=" ",
                     key=" ", canonical_id=" ", openstack_username=openstack_username,
                     openstack_password=openstack_password, openstack_project=openstack_project)
