a0e101ba14911c738c617edf0a2893bc782fa47e
Pierre Riteau <priteau@uchicago.edu>
2014-08-15 15:34:37 +0200

    TimingMiddleware was deprecated in favour of StatsdMiddleware

--- phantomweb/settings.py
+++ phantomweb/settings.py
@@ -122,7 +122,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django_statsd.middleware.TimingMiddleware',
+    'django_statsd.middleware.StatsdMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
